e286596b91617dce8ad6f8a8efa3468f8a34f807
Dev3773 <stepaniuk.sergiy@gmail.com>
2025-01-05 13:56:50 +0200

    Template modal window

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,48 +1,13 @@
-// const startRef = document.querySelector('.js-start');
-// const stopRef = document.querySelector('.js-stop');
-// const clearRef = document.querySelector('.js-clear');
-// const listRef = document.querySelector('.list');
+// Modal window handle
 
-// let fnId;
-// const murkup = `
-// <li class="item">Hallo Js !!!</li>
-// `;
+const openBtn = document.querySelector('.btn');
+const closeBtn = document.querySelector('.btn-x');
+const modal = document.querySelector('.modal-overlay');
 
-// startRef.addEventListener("click", () => {
-//   fnId = setInterval(() => {
-//     listRef.insertAdjacentHTML("afterbegin", murkup);
-//   }, 1000);
-// });
+openBtn.addEventListener('click', toggleModal);
+closeBtn.addEventListener('click', toggleModal);
 
-// stopRef.addEventListener("click", () => {
-//   clearInterval(fnId);
-// });
-
-// clearRef.addEventListener("click", () => {
-//   // listRef.innerHTML = ``;
-//   listRef.remove();
-// });
-
-// ******************************
-//   Date
-
-const date = new Date('2025-01-25T14:25');
-console.log(date.getTime());
-
-console.log(new Date(0));
-
-const time = Date.now();
-console.log(time);
-
-
-const startTime = Date.now();
-
-for (let i = 0; i < 100; i += 1) {
-  console.log(i);
+function toggleModal() {
+  modal.classList.toggle('is-open');
 }
-
-const endTime = Date.now();
-
-const difference = endTime - startTime;
-console.log('difference = ' + difference);
-
+  
